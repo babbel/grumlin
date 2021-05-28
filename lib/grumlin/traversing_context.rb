6254f300ec77a5b5a042de7ce15b9f2a69497b1a
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-05-28 12:30:32 +0200

    Extract Grumlin::Order

--- lib/grumlin/traversing_context.rb
+++ lib/grumlin/traversing_context.rb
@@ -2,23 +2,6 @@
 
 module Grumlin
   class TraversingContext
-    ORDER_DESC = { "@type": "g:Order", "@value": "desc" }.freeze
-    ORDER_ASC = { "@type": "g:Order", "@value": "desc" }.freeze
-
-    attr_reader :g
-
-    def initialize(traversal)
-      @g = traversal
-    end
-
-    def asc
-      ORDER_ASC
-    end
-
-    def desc
-      ORDER_DESC
-    end
-
     def self.V(id) # rubocop:disable Naming/MethodName
       { "@type": "g:Bytecode", "@value": { step: [["V", { "@type": "g:Int32", "@value": id }]] } }
     end
