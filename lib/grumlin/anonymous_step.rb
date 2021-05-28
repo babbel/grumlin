1a37cab066046bfdc45cd1a62d848a426bb3196c
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-05-28 15:52:19 +0200

    Extract AnonymousStep

---
+++ lib/grumlin/anonymous_step.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+module Grumlin
+  class AnonymousStep
+    attr_reader :name, :args
+
+    # TODO: add support for bytecode
+    def initialize(name, *args, previous_steps: [])
+      @name = name
+      @previous_steps = previous_steps
+      @args = args
+    end
+
+    %w[addV addE V E limit count drop property valueMap select from to as order by].each do |step|
+      define_method step do |*args|
+        add_step(step, args, previous_steps: steps)
+      end
+    end
+
+    alias addVertex addV
+    alias addEdge addE
+
+    # TODO: add support for next
+    # TODO: add support for iterate
+    # TODO: memoization
+    def toList # rubocop:disable Naming/MethodName
+      @client.query(*steps)
+    end
+
+    def inspect
+      "<Step #{self}>" # TODO: substitute bindings
+    end
+
+    # TODO: memoization
+    def to_s(*)
+      Translator.to_string(steps)
+    end
+
+    alias to_gremlin to_s
+
+    def steps
+      (@previous_steps + [self])
+    end
+
+    private
+
+    def add_step(step_name, args, previous_steps:)
+      self.class.new(step_name, *args, previous_steps: previous_steps)
+    end
+  end
+end
--- lib/grumlin/step.rb
+++ lib/grumlin/step.rb
@@ -1,46 +1,19 @@
 # frozen_string_literal: true
 
 module Grumlin
-  class Step
-    attr_reader :client, :name, :args
+  class Step < AnonymousStep
+    attr_reader :client
 
     # TODO: add support for bytecode
     def initialize(client, name, *args, previous_steps: [])
+      super(name, *args, previous_steps: previous_steps)
       @client = client
-      @name = name
-      @previous_steps = previous_steps
-      @args = args
     end
 
-    %w[addV addE V E limit count drop property valueMap select from to as order by].each do |step|
-      define_method step do |*args|
-        Step.new(@client, step, *args, previous_steps: steps)
-      end
-    end
+    private
 
-    alias addVertex addV
-    alias addEdge addE
-
-    # TODO: add support for next
-    # TODO: add support for iterate
-    # TODO: memoization
-    def toList # rubocop:disable Naming/MethodName
-      @client.query(*steps)
-    end
-
-    def inspect
-      "<Step #{self}>" # TODO: substitute bindings
-    end
-
-    # TODO: memoization
-    def to_s(*)
-      Translator.to_string(steps)
-    end
-
-    alias to_gremlin to_s
-
-    def steps
-      (@previous_steps + [self])
+    def add_step(step_name, args, previous_steps:)
+      self.class.new(@client, step_name, *args, previous_steps: previous_steps)
     end
   end
 end
