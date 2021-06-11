c8dd513b0a69223b778f6764787a9186e3703cc3
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-06-11 21:40:20 +0200

    Prepare Pratctical Gremlin test suite

--- spec/grumlin/practical_gremlin/0_import_data_spec.rb
+++ spec/grumlin/practical_gremlin/0_import_data_spec.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
-RSpec.describe CSVImporter, gremlin_server: true, timeout: 60 do
+RSpec.describe "Practical Gremlin: Import data", gremlin_server: true, timeout: 60 do # rubocop:disable RSpec/DescribeClass
   let(:nodes) { CSV.new(File.read("spec/fixtures/air_routes/nodes.csv"), headers: true) }
   let(:edges) { CSV.new(File.read("spec/fixtures/air_routes/edges.csv"), headers: true) }
-  let(:importer) { hdescribed_class.new(client, nodes, edges) }
+  let(:importer) { CSVImporter.new(client, nodes, edges) }
 
   describe "#import!" do
     it "imports the dataset" do # rubocop:disable RSpec/MultipleExpectations
+      g.V.drop.iterate
       importer.import!
 
       expect(g.V().count.toList[0]).to eq(3741)
---
+++ spec/grumlin/practical_gremlin/chapter_3_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe "Practical Gremlin: Chapter 3 specs" do  # rubocop:disable RSpec/DescribeClass
+  it "does something" do
+    p "Chapter 1"
+  end
+end
