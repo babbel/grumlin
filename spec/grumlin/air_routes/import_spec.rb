b3eea876ef05d0349102b727cb79d9e0169b3fb8
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-06-12 00:33:23 +0200

    Import nodes from graphml

--- spec/grumlin/air_routes/import_spec.rb
+++ spec/grumlin/air_routes/import_spec.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
-RSpec.describe "Import the air routes dataset", gremlin_server: true, timeout: 60 do # rubocop:disable RSpec/DescribeClass
-  let(:nodes) { CSV.new(File.read("spec/fixtures/air_routes/nodes.csv"), headers: true) }
-  let(:edges) { CSV.new(File.read("spec/fixtures/air_routes/edges.csv"), headers: true) }
+RSpec.describe "Import the air routes dataset", gremlin_server: true, timeout: 600 do # rubocop:disable RSpec/DescribeClass
+  let(:graphml) { File.read("spec/fixtures/air_routes/air-routes.graphml") }
 
   it "imports the dataset" do # rubocop:disable RSpec/MultipleExpectations
-    importer = CSVImporter.new(client, nodes, edges)
+    importer = GraphMLImporter.new(client, graphml)
     importer.import!
 
-    expect(g.V().count.toList[0]).to eq(3741)
+    expect(g.V().count.toList[0]).to eq(3618)
     expect(g.E().count.toList[0]).to eq(57_573)
   end
 end
---
+++ spec/support/graphml_importer.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+class GraphMLImporter
+  TYPES = {
+    "string" => :to_s,
+    "int" => :to_i,
+    "double" => :to_f
+  }.freeze
+
+  include Grumlin::U
+  include Grumlin::T
+
+  def initialize(client, graphml)
+    @client = client
+    @graphml = Nokogiri::XML(graphml)
+  end
+
+  def import!
+    import_nodes!
+    import_edges!
+  end
+
+  private
+
+  def import_nodes! # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
+    nodes.each_slice(100) do |slice|
+      t = g
+      slice.each do |node|
+        label = node.xpath("xmlns:data[@key='labelV']").text
+        next if label == "version"
+
+        t = t.addV(label).property(T.id, node.attributes["id"].value.to_i)
+        node.xpath("xmlns:data[not(@key='labelV')]").each do |attribute|
+          key = attribute.attributes["key"].value
+          cast_method = TYPES[properties[:node][key][0][:type]]
+          t = t.property(key, attribute.text.send(cast_method))
+        end
+      end
+      t.iterate
+    end
+  end
+
+  def import_edges!; end
+
+  def nodes
+    @graphml.xpath("//xmlns:graphml//xmlns:graph//xmlns:node")
+  end
+
+  def properties # rubocop:disable Metrics/AbcSize
+    @properties ||= @graphml.xpath("//xmlns:graphml//xmlns:key").map do |prop|
+      {
+        id: prop.attributes["id"].value,
+        name: prop.attributes["attr.name"].value,
+        type: prop.attributes["attr.type"].value,
+        for: prop.attributes["for"].value
+      }
+    end.group_by { |p| p[:for] }.transform_values { |v| v.group_by { |p| p[:id] } }.symbolize_keys # rubocop:disable Style/MultilineBlockChain
+  end
+
+  def g
+    Grumlin::Traversal.new(@client)
+  end
+end
