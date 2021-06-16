fb13fccd718c35f44abf638e169e3e327dfd3177
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-06-16 16:42:43 +0200

    Refactor tests

--- spec/grumlin/practical_gremlin/2_walking_spec.rb
+++ spec/grumlin/practical_gremlin/2_walking_spec.rb
@@ -1,324 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe "Practical Gremlin: Chapter 3 specs" do # rubocop:disable RSpec/DescribeClass
+RSpec.describe "Practical Gramlin: walking" do #  rubocop:disable RSpec/DescribeClass
   it "1" do
-    expect(g.V().hasLabel("airport").count.next).to eq(3374)
-  end
-
-  it "2" do
-    expect(g.V().has("code", "DFW").count.next).to eq(1)
-  end
-
-  it "3" do # rubocop:disable RSpec/MultipleExpectations
-    expect(g.V().hasLabel("airport").has("code", "DFW").count.next).to eq(1)
-    expect(g.V().has("airport", "code", "DFW").count.next).to eq(1)
-  end
-
-  it "4" do
-    expect(g.V().has("airport", "code",
-                     "DFW").values.toList).to eq(["US", "DFW", 13_401, "Dallas", 607, "KDFW",
-                                                  -97.0380020141602, "airport", "US-TX", 7, 32.896800994873,
-                                                  "Dallas/Fort Worth International Airport"])
-  end
-
-  it "5" do
-    expect(g.V().has("airport", "code", "DFW").values("city").toList).to eq(["Dallas"])
-  end
-
-  it "6" do
-    expect(g.V().has("airport", "code", "DFW").values("runways", "icao").toList).to eq(["KDFW", 7])
-  end
-
-  it "7" do # rubocop:disable RSpec/MultipleExpectations
-    expect(g.E().has("dist").count.next).to eq(43_400)
-    expect(g.V().has("region").count.next).to eq(3374)
-    expect(g.V().hasNot("region").count.next).to eq(245)
-    expect(g.V().not(Grumlin::U.has("region")).count.next).to eq(245)
-  end
-
-  it "8" do
-    expect(g.V().outE("route").count.next).to eq(43_400)
-  end
-
-  it "9" do
-    expect(g.E().hasLabel("route").count.next).to eq(43_400)
-  end
-
-  it "10" do # rubocop:disable RSpec/MultipleExpectations
-    result = { airport: 3374,
-               continent: 7,
-               country: 237,
-               version: 1 }
-    expect(g.V().groupCount.by(Grumlin::T.label).next).to eq(result)
-    expect(g.V().label.groupCount.next).to eq(result)
-    expect(g.V().group.by(Grumlin::T.label).by(Grumlin::U.count).next).to eq(result)
-  end
-
-  it "11" do # rubocop:disable RSpec/MultipleExpectations
-    result = { contains: 6748, route: 43_400 }
-    expect(g.E().groupCount.by(Grumlin::T.label).next).to eq(result)
-    expect(g.E().label.groupCount.next).to eq(result)
-  end
-
-  it "12" do # rubocop:disable RSpec/MultipleExpectations
-    result = {
-      PR: 6,
-      PT: 14,
-      PW: 1,
-      PY: 2,
-      QA: 1,
-      AE: 10,
-      AF: 4,
-      AG: 1,
-      AI: 1,
-      AL: 1,
-      AM: 2,
-      AO: 14,
-      AR: 36,
-      AS: 1,
-      AT: 6,
-      RE: 2,
-      AU: 124,
-      AW: 1,
-      AZ: 5,
-      RO: 14,
-      BA: 4,
-      BB: 1,
-      RS: 2,
-      BD: 7,
-      BE: 5,
-      RU: 120,
-      BF: 2,
-      BG: 4,
-      RW: 2,
-      BH: 1,
-      BI: 1,
-      BJ: 1,
-      BL: 1,
-      BM: 1,
-      BN: 1,
-      BO: 15,
-      SA: 26,
-      BQ: 3,
-      SB: 17,
-      BR: 115,
-      SC: 2,
-      BS: 18,
-      SD: 5,
-      SE: 39,
-      BT: 1,
-      SG: 1,
-      BW: 4,
-      SH: 2,
-      SI: 1,
-      BY: 2,
-      BZ: 13,
-      SK: 2,
-      SL: 1,
-      SN: 3,
-      SO: 5,
-      CA: 203,
-      SR: 1,
-      SS: 1,
-      CC: 1,
-      CD: 11,
-      ST: 1,
-      SV: 1,
-      CF: 1,
-      CG: 3,
-      CH: 5,
-      SX: 1,
-      CI: 1,
-      SY: 2,
-      SZ: 1,
-      CK: 6,
-      CL: 17,
-      CM: 5,
-      CN: 209,
-      CO: 50,
-      CR: 13,
-      TC: 4,
-      TD: 1,
-      CU: 12,
-      CV: 7,
-      TG: 1,
-      TH: 32,
-      CW: 1,
-      CX: 1,
-      CY: 3,
-      TJ: 4,
-      CZ: 5,
-      TL: 1,
-      TM: 1,
-      TN: 8,
-      TO: 1,
-      TR: 48,
-      TT: 2,
-      DE: 33,
-      TV: 1,
-      TW: 9,
-      TZ: 8,
-      DJ: 1,
-      DK: 8,
-      DM: 1,
-      DO: 7,
-      UA: 15,
-      UG: 4,
-      UK: 58,
-      DZ: 29,
-      US: 579,
-      EC: 15,
-      EE: 3,
-      EG: 10,
-      EH: 2,
-      UY: 2,
-      UZ: 11,
-      ER: 1,
-      VC: 1,
-      ES: 42,
-      ET: 14,
-      VE: 24,
-      VG: 2,
-      VI: 2,
-      VN: 21,
-      VU: 26,
-      FI: 20,
-      FJ: 10,
-      FK: 2,
-      FM: 4,
-      FO: 1,
-      FR: 58,
-      WF: 2,
-      GA: 2,
-      WS: 1,
-      GD: 1,
-      GE: 3,
-      GF: 1,
-      GG: 2,
-      GH: 5,
-      GI: 1,
-      GL: 14,
-      GM: 1,
-      GN: 1,
-      GP: 1,
-      GQ: 2,
-      GR: 39,
-      GT: 2,
-      GU: 1,
-      GW: 1,
-      GY: 2,
-      HK: 1,
-      HN: 6,
-      HR: 8,
-      HT: 2,
-      YE: 9,
-      HU: 2,
-      ID: 67,
-      YT: 1,
-      IE: 7,
-      IL: 5,
-      IM: 1,
-      IN: 73,
-      ZA: 20,
-      IQ: 6,
-      IR: 44,
-      IS: 5,
-      IT: 36,
-      ZM: 8,
-      JE: 1,
-      ZW: 3,
-      JM: 2,
-      JO: 2,
-      JP: 63,
-      KE: 14,
-      KG: 2,
-      KH: 3,
-      KI: 2,
-      KM: 1,
-      KN: 2,
-      KP: 1,
-      KR: 15,
-      KS: 1,
-      KW: 1,
-      KY: 3,
-      KZ: 20,
-      LA: 8,
-      LB: 1,
-      LC: 2,
-      LK: 6,
-      LR: 2,
-      LS: 1,
-      LT: 3,
-      LU: 1,
-      LV: 1,
-      LY: 10,
-      MA: 14,
-      MD: 1,
-      ME: 2,
-      MF: 1,
-      MG: 13,
-      MH: 2,
-      MK: 1,
-      ML: 1,
-      MM: 14,
-      MN: 10,
-      MO: 1,
-      MP: 2,
-      MQ: 1,
-      MR: 3,
-      MS: 1,
-      MT: 1,
-      MU: 2,
-      MV: 8,
-      MW: 2,
-      MX: 59,
-      MY: 34,
-      MZ: 10,
-      NA: 4,
-      NC: 1,
-      NE: 1,
-      NF: 1,
-      NG: 18,
-      NI: 1,
-      NL: 5,
-      NO: 49,
-      NP: 10,
-      NR: 1,
-      NZ: 25,
-      OM: 4,
-      PA: 5,
-      PE: 20,
-      PF: 30,
-      PG: 26,
-      PH: 38,
-      PK: 21,
-      PL: 13,
-      PM: 1
-    }
-    expect(g.V().hasLabel("airport").groupCount.by("country").next).to eq(result)
-    expect(g.V().hasLabel("country").group.by("code").by(Grumlin::U.out.count).next).to eq(result.merge(AD: 0, LI: 0,
-                                                                                                        MC: 0, PN: 0,
-                                                                                                        SM: 0))
-  end
-
-  it "13" do
-    expect(g.V().hasLabel("continent").group.by("code").by(Grumlin::U.out.count).next).to eq({ EU: 583, AS: 932,
-                                                                                               NA: 978, OC: 284,
-                                                                                               AF: 294, AN: 0,
-                                                                                               SA: 303 })
-  end
-
-  it "14" do
-    expect(g.V().hasLabel("airport").groupCount.by("country").select("FR").next).to eq(58)
-  end
-
-  it "15" do
-    expect(g.V().hasLabel("airport").groupCount.by("country").select("FR", "GR", "BE").next).to eq({ BE: 5,
-                                                                                                     FR: 58,
-                                                                                                     GR: 39 })
-  end
-
-  it "16" do
     expect(g.V().has("airport", "code", "AUS").out.values("code").fold.next).to eq(%w[YYZ
                                                                                       LHR
                                                                                       FRA
@@ -380,11 +63,11 @@ RSpec.describe "Practical Gremlin: Chapter 3 specs" do # rubocop:disable RSpec/D
                                                                                       DTW])
   end
 
-  it "17" do
+  it "2" do
     expect(g.V().has("airport", "code", "AUS").out("route").out("route").values("code").count.next).to eq(5942)
   end
 
-  it "18" do
+  it "3" do
     expect(g.V().has("airport", "code", "LCY").in("route").values("code").toList).to eq(%w[IBZ
                                                                                            AGP
                                                                                            PMI
@@ -430,7 +113,7 @@ RSpec.describe "Practical Gremlin: Chapter 3 specs" do # rubocop:disable RSpec/D
                                                                                            BRE])
   end
 
-  it "19" do
+  it "4" do
     expect(g.V().has("code", "LHR").out("route").has("country",
                                                      "US").values("code").toList).to eq(%w[PDX CLT ATL AUS BOS BWI DFW
                                                                                            IAD IAH JFK LAX MIA MSP ORD
@@ -438,11 +121,11 @@ RSpec.describe "Practical Gremlin: Chapter 3 specs" do # rubocop:disable RSpec/D
                                                                                            LAS DEN MSY EWR PHL DTW])
   end
 
-  it "20" do
+  it "5" do
     expect(g.V().has("airport", "code", "LCY").outE.inV.path.count.next).to eq(42)
   end
 
-  it "21" do # rubocop:disable RSpec/MultipleExpectations
+  it "6" do # rubocop:disable RSpec/MultipleExpectations
     expect(
       g.V().has("airport", "code", "LCY").outE.inV
             .path.by("code").by("dist").count.next
@@ -463,13 +146,13 @@ RSpec.describe "Practical Gremlin: Chapter 3 specs" do # rubocop:disable RSpec/D
     ).to eq(42)
   end
 
-  it "22" do
+  it "7" do
     expect(
       g.V(3).out.limit(5).path.by(Grumlin::U.values("code", "city").fold).count.next
     ).to eq(5)
   end
 
-  it "23" do
+  it "8" do
     expect(
       g.V(3).out.limit(5).path.by(Grumlin::U.out.count.fold).count.next
     ).to eq(5)
