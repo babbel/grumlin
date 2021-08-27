cf29b11881ae1cb24a7ce4756852dab35b647099
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-08-27 14:43:46 +0200

    Minor renames

--- lib/grumlin/t.rb
+++ lib/grumlin/t.rb
@@ -3,8 +3,8 @@
 module Grumlin
   module T
     class << self
-      T_ID = { :@type => "g:T", :@value => "id" }.freeze # TODO: replace with a class?
-      T_LABEL = { :@type => "g:T", :@value => "label" }.freeze # TODO: replace with a class?
+      T_ID = { "@type": "g:T", "@value": "id" }.freeze # TODO: replace with a class?
+      T_LABEL = { "@type": "g:T", "@value": "label" }.freeze # TODO: replace with a class?
 
       def id
         T_ID
