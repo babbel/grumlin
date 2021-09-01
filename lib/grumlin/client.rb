f50324edbf9fd970a557389cbe2c77b94bf74fa1
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-09-01 12:04:01 +0200

    Hotfix client (#22)
    * Fix
    
    * Do not run rspec on prepush
    
    * Bump version

--- lib/grumlin/client.rb
+++ lib/grumlin/client.rb
@@ -49,8 +49,10 @@ module Grumlin
     end
 
     def close
-      @transport.close
-      raise ResourceLeakError, "Request list is not empty: #{requests}" if @request_dispatcher.requests.any?
+      @transport&.close
+      if @request_dispatcher&.requests&.any?
+        raise ResourceLeakError, "Request list is not empty: #{@request_dispatcher.requests}"
+      end
 
       reset!
     end
