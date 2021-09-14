ad96bbf58b7f105541da2233e0744551706a2ecd
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-09-14 15:41:18 +0200

    Refactor predicates and add P.neq

--- lib/grumlin/p.rb
+++ lib/grumlin/p.rb
@@ -3,13 +3,31 @@
 module Grumlin
   module P
     module P
+      class Predicate < TypedValue
+        def initialize(name, args)
+          super("P", build_value(name, args))
+        end
+
+        private
+
+        def build_value(name, args)
+          type, args = cast_args(args)
+          { predicate: name, value: { "@type": type, "@value": args } }
+        end
+
+        def cast_args(args)
+          if args.count > 1
+            ["g:List", args]
+          else
+            ["g:String", args[0]] # TODO: support other types
+          end
+        end
+      end
+
       # TODO: support more predicates
-      %w[within].each do |step|
-        define_method step do |*args|
-          { # TODO: replace with a TypedValue?
-            "@type": "g:P",
-            "@value": { predicate: "within", value: { "@type": "g:List", "@value": args } }
-          }
+      %w[neq within].each do |predicate|
+        define_method predicate do |*args|
+          Predicate.new(predicate, args)
         end
       end
     end
