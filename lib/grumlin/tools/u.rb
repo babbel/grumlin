d9dd577ca2c52f24ad3a0a9bfa7408e62ca08765
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-09-24 11:37:02 +0200

    Add inE step

--- lib/grumlin/tools/u.rb
+++ lib/grumlin/tools/u.rb
@@ -4,7 +4,7 @@ module Grumlin
   module Tools
     module U
       # TODO: add other start steps
-      SUPPORTED_STEPS = %i[V addV count fold has id inV label out outV project repeat timeLimit unfold valueMap
+      SUPPORTED_STEPS = %i[V addV count fold has id inE inV label out outV project repeat timeLimit unfold valueMap
                            values].freeze
 
       class << self
