d5b6992ed859f34e3e92f6b75187a1280dfcc2d9
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2021-12-02 15:58:02 +0100

    Fix properties shortcuts

--- lib/grumlin/shortcuts/properties.rb
+++ lib/grumlin/shortcuts/properties.rb
@@ -5,13 +5,13 @@ module Grumlin
     module Properties
       extend Grumlin::Shortcuts
 
-      shortcut :props do |**props|
+      shortcut :props do |props = {}|
         props.reduce(self) do |tt, (prop, value)|
           tt.property(prop, value)
         end
       end
 
-      shortcut :hasAll do |**props|
+      shortcut :hasAll do |props = {}|
         props.reduce(self) do |tt, (prop, value)|
           tt.has(prop, value)
         end
