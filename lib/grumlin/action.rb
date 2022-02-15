0146b5823ca04c4bf8fa30097e6c8e28c190d61c
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2022-02-15 17:54:05 +0100

    Add Action

---
+++ lib/grumlin/action.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+module Grumlin
+  class Action
+    START_STEPS = Grumlin.definitions.dig(:steps, :start).map(&:to_sym).freeze # TODO: add validation
+    CONFIGURATION_STEPS = Grumlin.definitions.dig(:steps, :configuration).map(&:to_sym).freeze # TODO: add validation
+    REGULAR_STEPS = Grumlin.definitions.dig(:steps, :regular).map(&:to_sym).freeze
+
+    ALL_STEPS = START_STEPS + CONFIGURATION_STEPS + REGULAR_STEPS
+
+    attr_reader :name, :args, :params, :shortcuts, :next_step, :configuration_steps, :previous_step
+
+    def initialize(name, args: [], params: {}, previous_step: nil, shortcuts: {}, pool: Grumlin.default_pool)
+      @name = name.to_sym
+      @args = args
+      @params = params
+      @previous_step = previous_step
+      @shortcuts = shortcuts
+      @pool = pool
+    end
+
+    ALL_STEPS.each do |step|
+      define_method step do |*args, **params|
+        step(step, *args, **params)
+      end
+    end
+
+    def step(name, *args, **params)
+      Action.new(name, args: args, params: params, previous_step: self, shortcuts: @shortcuts, pool: @pool)
+    end
+
+    def configuration_step?
+      CONFIGURATION_STEPS.include?(@name)
+    end
+
+    def start_step?
+      START_STEPS.include?(@name)
+    end
+
+    def regular_step?
+      REGULAR_STEPS.include?(@name)
+    end
+
+    def supported_step?
+      ALL_STEPS.include?(@name)
+    end
+
+    def shortcut?
+      @shortcuts.key?(@name)
+    end
+
+    def arguments
+      @arguments ||= [*@args].tap do |args|
+        args << @params if @params.any?
+      end
+    end
+
+    def method_missing(name, *args, **params)
+      return step(name, *args, **params) if @shortcuts.key?(name)
+
+      super
+    end
+
+    def ==(other)
+      self.class == other.class &&
+        @name == other.name &&
+        @args == other.args &&
+        @params == other.params &&
+        @previous_step == other.previous_step
+    end
+
+    private
+
+    def respond_to_missing?(name, _include_private = false)
+      @shortcuts.key?(name)
+    end
+  end
+end
---
+++ spec/grumlin/action_spec.rb
@@ -0,0 +1,212 @@
+# frozen_string_literal: true
+
+RSpec.describe Grumlin::Action do
+  let(:action) { described_class.new(name, args: args, params: params, shortcuts: shortcuts) }
+  let(:name) { :V }
+  let(:args) { [] }
+  let(:params) { {} }
+  let(:shortcuts) { {} }
+
+  describe "chaining" do
+    context "when no shortcuts are used" do
+      it "chains" do
+        configuration_step = described_class.new(:withSideEffect, args: [:a], params: { a: 1 })
+        expect(configuration_step.previous_step).to be_nil
+        expect(configuration_step).to be_an(described_class)
+        expect(configuration_step.name).to eq(:withSideEffect)
+        expect(configuration_step.arguments).to eq([:a, { a: 1 }])
+
+        start_step = configuration_step.V
+        expect(start_step).to be_an(described_class)
+        expect(start_step.name).to eq(:V)
+        expect(start_step.arguments).to be_empty
+
+        regular_step = start_step.has(:property, :value)
+        expect(regular_step).to be_an(described_class)
+        expect(regular_step.name).to eq(:has)
+        expect(regular_step.arguments).to eq(%i[property value])
+
+        expect(regular_step.previous_step).to equal(start_step)
+        expect(regular_step.previous_step.previous_step).to equal(configuration_step)
+      end
+    end
+
+    context "when shortcuts are used" do
+      subject { action.foo(:arg1, :arg2, param1: 1, param2: 2) }
+
+      let(:shortcuts) { { foo: -> {} } }
+
+      context "when shortcut is empty" do
+        it "returns an Action" do
+          expect(subject).to be_an(described_class)
+        end
+
+        it "assigns passes args and params to the new Action" do
+          expect(subject.arguments).to eq([:arg1, :arg2, { param1: 1, param2: 2 }])
+        end
+
+        it "assigns previous_step" do
+          expect(subject.previous_step).to equal(action)
+        end
+      end
+    end
+  end
+
+  describe "#step" do
+    subject { action.step("step", :arg1, :arg2, param1: 1, param2: 2) }
+
+    it "returns an Action" do
+      expect(subject).to be_an(described_class)
+    end
+
+    it "assigns passes args and params to the new Action" do
+      expect(subject.arguments).to eq([:arg1, :arg2, { param1: 1, param2: 2 }])
+    end
+
+    it "assigns previous_step" do
+      expect(subject.previous_step).to equal(action)
+    end
+  end
+
+  describe "#configuration_step?" do
+    subject { action.configuration_step? }
+
+    context "when step is a configuration step" do
+      let(:name) { "withSideEffect" }
+
+      it "returns true" do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context "when step is not a configuration_step" do
+      it "returns false" do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe "#start_step?" do
+    subject { action.start_step? }
+
+    context "when step a start step" do
+      it "returns true" do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context "when step is not a configuration_step" do
+      let(:name) { "withSideEffect" }
+
+      it "returns false" do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe "#regular_step?" do
+    subject { action.regular_step? }
+
+    context "when step a regular step" do
+      let(:name) { :has }
+
+      it "returns true" do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context "when step is not a configuration_step" do
+      let(:name) { "withSideEffect" }
+
+      it "returns false" do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe "#supported_step?" do
+    subject { action.supported_step? }
+
+    context "when step is supported" do
+      it "returns true" do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context "when step is not supported" do
+      let(:name) { "some_step" }
+
+      it "returns false" do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe "#shortcut?" do
+    subject { action.shortcut? }
+
+    context "when step is a shortcut" do
+      let(:shortcuts) { { shortcut: -> {} } }
+      let(:name) { :shortcut }
+
+      it "returns true" do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context "when step is no a shortcut" do
+      it "returns false" do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+
+  describe "#arguments" do
+    subject { action.arguments }
+
+    context "when args and params are empty" do
+      it "returns an empty list" do
+        expect(subject).to be_empty
+      end
+    end
+
+    context "when params are empty" do
+      let(:args) { %i[arg1 arg2] }
+
+      it "returns args" do
+        expect(subject).to eq(args)
+      end
+    end
+
+    context "when args and params are not empty" do
+      let(:args) { %i[arg1 arg2] }
+      let(:params) { { param1: 1, param2: 2 } }
+
+      it "returns args" do
+        expect(subject).to eq([:arg1, :arg2, { param1: 1, param2: 2 }])
+      end
+    end
+  end
+
+  describe "#==" do
+    subject { action == other_action }
+
+    context "when name, args, params and previous step are equal" do
+      let(:action) { described_class.new(:V).has(:property, :value).where(described_class.new(:has, args: %i[property value])) }
+      let(:other_action) { described_class.new(:V).has(:property, :value).where(described_class.new(:has, args: %i[property value])) }
+
+      it "returns true" do
+        expect(subject).to be_truthy
+      end
+    end
+
+    context "when something is not equal" do
+      let(:action) { described_class.new(:V).has(:property, :value).where(described_class.new(:has, args: %i[property value])) }
+      let(:other_action) { described_class.new(:V).has(:property, :value).where(described_class.new(:V, args: [:id])) }
+
+      it "returns false" do
+        expect(subject).to be_falsey
+      end
+    end
+  end
+end
