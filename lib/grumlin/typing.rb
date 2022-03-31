d1232cd86e0ee6f17fe8f020fe516f441e5091a9
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2022-03-31 17:57:34 +0200

    Properly handle T.id and T.label in results of the elementMap step

--- lib/grumlin/typing.rb
+++ lib/grumlin/typing.rb
@@ -18,7 +18,7 @@ module Grumlin
       # "g:VertexProperty"=> ->(value) { value }, # TODO: implement me
       "g:TraversalMetrics" => ->(value) { cast_map(value[:@value]) },
       "g:Metrics" => ->(value) { cast_map(value[:@value]) },
-      "g:T" => ->(value) { value.to_sym }
+      "g:T" => ->(value) { T.public_send(value) }
     }.freeze
 
     CASTABLE_TYPES = [Hash, String, Integer, TrueClass, FalseClass].freeze
