96d4f407e399928e6dd37958856b98fa0ae82b48
Gleb Sinyavskiy <gsinyavskiy@babbel.com>
2023-08-15 12:51:11 +0200

    Fix TextP predicates (#139)

--- lib/grumlin/expressions/text_p.rb
+++ lib/grumlin/expressions/text_p.rb
@@ -4,7 +4,7 @@ class Grumlin::Expressions::TextP < Grumlin::Expressions::P
   class << self
     [:containing, :endingWith, :notContaining, :notEndingWith, :notStartingWith, :startingWith].each do |predicate|
       define_method predicate do |*args|
-        P::Predicate.new("TextP", predicate, value: args[0])
+        Grumlin::Expressions::P::Predicate.new("TextP", predicate, value: args[0])
       end
     end
   end
